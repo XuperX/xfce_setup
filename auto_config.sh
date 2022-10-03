c1618d894dd653bb01a5ce33f3f23e9d6386db4f
X-name <36501176+XuperX@users.noreply.github.com>
2022-10-03 10:59:23 +0100

    Create auto_config.sh

---
+++ auto_config.sh
@@ -0,0 +1,11 @@
+# keyboard shortcuts
+## ranger terminal file manager
+echo "alias rr='ranger'">>.bashrc
+
+## translate to chinese
+echo "alias zh='trans en:zh'">>.bashrc
+source .bashrc
+
+## git 
+echo "alias ghpd='bash ~/TOKEN/push_phd_diary.sh'">>.bashrc
+source .bashrc
